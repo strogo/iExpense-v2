dabb34c31f76c09e605a9d3d256155db96501290
pavelsurovy <surolm@centrum.sk>
2021-12-05 08:15:04 +0100

    Initial Commit

---
+++ iExpense-v2/iExpense_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_v2App.swift
+//  iExpense-v2
+//
+//  Created by Palino on 05/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
